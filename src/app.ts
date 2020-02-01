989905aedb71b64594cee0f67247461832439de7
Matt Seto <seanseto@gmail.com>
2020-02-01 12:12:49 -0800

    use cookie-parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from 'express'
+import cookieParser from 'cookie-parser'
 import routes from './routes/index.route'
 import { resolve } from 'path'
 
 const app: Application = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(cookieParser())
 app.set('view engine', 'ejs')
 app.set('views', resolve(__dirname, '../views'))
 app.use('/', routes)
