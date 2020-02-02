c1bab52da008c747b0313b818795aff8b4b4ad82
Matt Seto <seanseto@gmail.com>
2020-02-02 00:59:48 -0800

    custom error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response, NextFunction } from 'express'
 import cookieParser from 'cookie-parser'
 import routes from './routes/index.route'
 import { resolve } from 'path'
@@ -10,5 +10,11 @@ app.use(cookieParser())
 app.set('view engine', 'ejs')
 app.set('views', resolve(__dirname, '../views'))
 app.use('/', routes)
+app.use('/*', (req: Request, res: Response, next: NextFunction) => {
+	res.status(404).send('Page not found')
+})
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+	console.log(error.message)
+})
 
 export default app
