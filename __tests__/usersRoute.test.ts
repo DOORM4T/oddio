19a13f4d0a4613e19aa9f0d3868b6fab991eade2
Matt Seto <seanseto@gmail.com>
2020-02-02 00:59:16 -0800

    user auth testing

--- __tests__/usersRoute.test.ts
+++ __tests__/usersRoute.test.ts
@@ -1,21 +1,50 @@
-import supertest, { SuperTest } from 'supertest'
+require('dotenv').config()
+import supertest, { SuperTest, Response } from 'supertest'
 import app from '../src/app'
+import injectMongo, { client } from '../src/db'
 
-const request: SuperTest<any> = supertest(app)
+const request: SuperTest<any> = supertest.agent(app)
+
+beforeAll(async () => {
+	await injectMongo()
+})
+
+afterAll(async () => {
+	await client.close()
+})
 
 describe('Users API', () => {
 	describe('User Authentication', () => {
 		it('registers new account successfully', (done) => {
 			request
 				.post('/auth/register')
-				.send({ email: 'firstman@example.com', password: 'C00lc4t123$%^' })
+				.send({
+					email: 'firstman@example.com',
+					username: 'firstman',
+					password: 'C00lc4t123$%^',
+				})
 				.expect(200, done)
+		})
+
+		it('error on registering with pre-existing email or username', (done) => {
+			request
+				.post('/auth/register')
+				.send({
+					email: 'firstman@example.com',
+					username: 'firstman',
+					password: 'C00lc4t123$%^',
+				})
+				.expect(400, done)
 		})
 
 		it('error on invalid user registration', (done) => {
 			request
 				.post('/auth/register')
-				.send({ email: 'bad email', password: 'abc123' })
+				.send({
+					email: 'bad email',
+					username: 'bad username',
+					password: 'abc123',
+				})
 				.expect(400, done)
 		})
 
@@ -43,39 +72,39 @@ describe('Users API', () => {
 		it('logs out successfully', (done) => {
 			request
 				.post('/auth/logout')
-				.expect(200, done)
+				.expect(200)
+				.end((err: Error, res: Response) => {
+					if (err) throw err
+					console.log(res.error)
+
+					done()
+				})
 		})
 
-		it('error on logout attempt when not logged in', (done) => {
-			request
-				.post('/auth/logout')
-				.expect(400, done)
-		})
+		// 	it('error on logout attempt when not logged in', (done) => {
+		// 		request.post('/auth/logout').expect(400, done)
+		// 	})
 
-		it('can delete pre-existing users', (done) => {
-			request
-				.post('/auth/deleteaccount')
-				.send({ password: 'C00lc4t123$%^' })
-		})
+		// 	it('can delete pre-existing users', (done) => {
+		// 		request.post('/auth/deleteuser').send({ password: 'C00lc4t123$%^' })
+		// 	})
 
-		it('fails to delete non-existant users', (done) => {
-			request
-				.post('/auth/deleteaccount')
-				.send({ password: 'C00lc4t123$%^' })
-		})
+		// 	it('fails to delete non-existant users', (done) => {
+		// 		request.post('/auth/deleteuser').send({ password: 'C00lc4t123$%^' })
+		// 	})
+		// })
+
+		// describe('User Info', () => {
+		// 	it('gets list of users', (done) => {})
+		// 	it('gets user by ID', (done) => {})
+		// })
+
+		// describe('User Activity', () => {
+		// 	it('successfully follow other users', (done) => {})
+		// 	it('gets user by ID', (done) => {})
+		// })
+
+		// describe('Account Management', () => {
+		// 	it('deletes user account successfully', (done) => {})
 	})
-
-	// describe('User Info', () => {
-	// 	it('gets list of users', (done) => {})
-	// 	it('gets user by ID', (done) => {})
-	// })
-
-	// describe('User Activity', () => {
-	// 	it('successfully follow other users', (done) => {})
-	// 	it('gets user by ID', (done) => {})
-	// })
-
-	// describe('Account Management', () => {
-	// 	it('deletes user account successfully', (done) => {})
-	// })
 })
