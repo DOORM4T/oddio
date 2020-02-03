9d1c73be55d3bad1b22b87a8ea6e0f4e3de0da54
Matt Seto <seanseto@gmail.com>
2020-02-02 20:27:23 -0800

    authentication for usersRoute testing

--- __tests__/usersRoute.test.ts
+++ __tests__/usersRoute.test.ts
@@ -3,14 +3,17 @@ import supertest, { SuperTest, Response } from 'supertest'
 import app from '../src/app'
 import injectMongo, { client } from '../src/db'
 
-const request: SuperTest<any> = supertest.agent(app)
-
-beforeAll(async () => {
-	await injectMongo()
+let request: SuperTest<any> = supertest(app)
+let authCookie: Array<string>
+beforeAll(async (done) => {
+	const mongoURI = process.env.MONGO_JEST_URI || ''
+	await injectMongo(mongoURI)
+	done()
 })
 
-afterAll(async () => {
+afterAll(async (done) => {
 	await client.close()
+	done()
 })
 
 describe('Users API', () => {
@@ -52,14 +55,25 @@ describe('Users API', () => {
 			request
 				.post('/auth/login')
 				.send({ email: 'firstman@example.com', password: 'C00lc4t123$%^' })
-				.expect(200, done)
+				.expect(200)
+				.end((err: Error, res: Response) => {
+					if (err) throw err
+					authCookie = res.get('Set-Cookie')
+					done()
+				})
 		})
 
-		it('gives session token on login', (done) => {
+		it('logs in pre-existing user', (done) => {
 			request
 				.post('/auth/login')
 				.send({ email: 'firstman@example.com', password: 'C00lc4t123$%^' })
-				.expect(200, done)
+				.expect(200)
+				.end((err: Error, res: Response) => {
+					if (err) throw err
+					authCookie = res.get('Set-Cookie')
+					if (!authCookie) throw new Error('Unable to get authCookie')
+					done()
+				})
 		})
 
 		it('error on invalid login', (done) => {
@@ -72,39 +86,45 @@ describe('Users API', () => {
 		it('logs out successfully', (done) => {
 			request
 				.post('/auth/logout')
+				.set({ Cookie: authCookie })
 				.expect(200)
 				.end((err: Error, res: Response) => {
 					if (err) throw err
-					console.log(res.error)
-
 					done()
 				})
 		})
 
-		// 	it('error on logout attempt when not logged in', (done) => {
-		// 		request.post('/auth/logout').expect(400, done)
-		// 	})
+		it('forbidden to logout when not logged in', (done) => {
+			request.post('/auth/logout').expect(403, done)
+		})
 
-		// 	it('can delete pre-existing users', (done) => {
-		// 		request.post('/auth/deleteuser').send({ password: 'C00lc4t123$%^' })
-		// 	})
+		it('can delete pre-existing users', (done) => {
+			request
+				.post('/auth/deleteuser')
+				.set({ Cookie: authCookie })
+				.send({ password: 'C00lc4t123$%^' })
+				.expect(200, done)
+		})
 
-		// 	it('fails to delete non-existant users', (done) => {
-		// 		request.post('/auth/deleteuser').send({ password: 'C00lc4t123$%^' })
-		// 	})
-		// })
-
-		// describe('User Info', () => {
-		// 	it('gets list of users', (done) => {})
-		// 	it('gets user by ID', (done) => {})
-		// })
-
-		// describe('User Activity', () => {
-		// 	it('successfully follow other users', (done) => {})
-		// 	it('gets user by ID', (done) => {})
-		// })
-
-		// describe('Account Management', () => {
-		// 	it('deletes user account successfully', (done) => {})
+		it('forbidden to delete non-existant users', (done) => {
+			request
+				.post('/auth/deleteuser')
+				.send({ password: 'password of nonexistant user' })
+				.expect(403, done)
+		})
 	})
+
+	// describe('User Info', () => {
+	// 	it('gets list of users',  (done) => {})
+	// 	it('gets user by ID',  (done) => {})
+	// })
+
+	// describe('User Activity', () => {
+	// 	it('successfully follow other users',  (done) => {})
+	// 	it('gets user by ID',  (done) => {})
+	// })
+
+	// describe('Account Management', () => {
+	// 	it('deletes user account successfully',  (done) => {})
+	// })
 })
