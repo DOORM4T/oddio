79c17724629457bd40501ac65f50b65ffbb4dff5
Matt Seto <seanseto@gmail.com>
2020-02-17 08:08:09 -0800

    Add dashboard page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import AOS, { AosOptions } from 'aos'
 import 'aos/dist/aos.css'
 import './main.scss'
-
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
 
 export default function App() {
 	useEffect(() => {
@@ -22,6 +22,7 @@ export default function App() {
 		<Router>
 			<Switch>
 				<Route exact path="/" render={() => <Home />} />
+				<Route path="/dashboard" render={() => <Dashboard />} />
 				<Route path="/login" render={() => <Login />} />
 				<Route path="/register" render={() => <Register />} />
 				<Route path="/*" render={() => <div>404</div>} />
---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+import React, { useContext } from 'react'
+import Header from '../components/Header'
+
+export default function Dashboard() {
+	return (
+		<article>
+			<Header title="Dashboard" icon="👤" />
+		</article>
+	)
+}
