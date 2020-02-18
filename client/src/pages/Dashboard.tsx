496275408afd8a4041bb687f8b3e584eb4701ed4
Matt Seto <seanseto@gmail.com>
2020-02-17 22:44:20 -0800

    add dashboard sections

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Header from '../components/Header'
 import AddSoundForm from '../containers/AddSoundForm'
 import useUserInfoFromCookie from '../util/useUserInfoFromCookie'
 
 export default function Dashboard() {
-	const [userInfo] = useUserInfoFromCookie()
+	const [userInfo] = useUserInfoFromCookie(true)
+	// const [mySounds, setMySounds] = useState()
+	// const [soundsILike, setSoundsILike] = useState()
+
+	// useEffect(() => {
+	// 	const mySoundsPromises = userInfo.sounds.map((soundId) =>
+	// 		fetch(`/api/sounds/${soundId}`)
+	// 	)
+	// 	Promise.all(mySoundsPromises)
+	// 		.then((res) =>
+	// 			res.forEach((response) => {
+	// 				return response.json()
+	// 			})
+	// 		)
+	// 		.then(console.log)
+	// }, [userInfo])
 
 	return (
 		<article>
-			<Header
-				title={`${userInfo && userInfo.username}'s Dashboard`}
-				icon="👤"
-			/>
+			<Header title="Dashboard" icon="👤" />
 			<AddSoundForm />
+			<section>
+				<h1>My Sounds</h1>
+			</section>
+			<section>
+				<h1>Favorites</h1>
+			</section>
 		</article>
 	)
 }
