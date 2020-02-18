ce89bb36fc7e50deaa2b0e30b02cd917f2889dea
Matt Seto <seanseto@gmail.com>
2020-02-17 22:43:47 -0800

    Add initial state

--- client/src/util/useUserInfoFromCookie.tsx
+++ client/src/util/useUserInfoFromCookie.tsx
@@ -2,11 +2,24 @@ import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { decode } from 'jsonwebtoken'
 
-export default function useUserInfoFromCookie() {
-	const [userData, setUserData] = useState()
+export default function useUserInfoFromCookie(enableRedirect: boolean = false) {
+	const [userData, setUserData] = useState<User>({
+		_id: '',
+		email: '',
+		joined: '',
+		private: true,
+		sounds: [],
+		soundsFamed: [],
+		username: '',
+	})
 	const history = useHistory()
 
 	useEffect(() => {
+		if (!document.cookie) {
+			if (enableRedirect) history.push('/login')
+			return
+		}
+
 		const decodedUserToken: any = decode(
 			document.cookie.slice('authToken='.length)
 		)
@@ -27,4 +40,14 @@ export default function useUserInfoFromCookie() {
 	}, [])
 
 	return [userData]
+}
+
+type User = {
+	_id: string
+	username: string
+	soundsFamed: string[]
+	sounds: string[]
+	private: boolean
+	email: string
+	joined: string
 }
