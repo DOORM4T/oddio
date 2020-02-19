0ec974905d9b27c27787df38ac0adbab96fda2dc
Matt Seto <seanseto@gmail.com>
2020-02-19 13:58:41 -0800

    add type prop

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -4,13 +4,19 @@ import styles from './Button.module.scss'
 interface ButtonProps {
 	color?: string
 	children: string
+	type?: 'button' | 'submit' | 'reset'
 }
 
-export default function Button({ color = '', children }: ButtonProps) {
+export default function Button({
+	color = '',
+	children,
+	type = 'button',
+}: ButtonProps) {
 	return (
 		<button
 			className={styles.button}
 			style={{ [color ? 'backgroundColor' : '']: color }}
+			type={type}
 		>
 			{children}
 		</button>
