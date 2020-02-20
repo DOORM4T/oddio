22eb658466af525dd6305ee34ad858bd8f593bf2
Matt Seto <seanseto@gmail.com>
2020-02-19 22:29:07 -0800

    create SoundCatalogContainer component

--- client/src/containers/SoundCatalogContainer.tsx
+++ client/src/containers/SoundCatalogContainer.tsx
@@ -1,66 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import Catalog from '../components/Catalog'
-
-interface SoundCatalogContainerProps {
-	query?: string
-}
+import Sound from '../util/sound'
 
 export default function SoundCatalogContainer({
 	query = '',
-}: SoundCatalogContainerProps) {
+}: SoundsCatalogContainerProps) {
 	const [sounds, setSounds] = useState<Sound[]>([])
 
 	useEffect(() => {
-		async function getSounds(): Promise<Sound[]> {
-			try {
-				const response = await fetch(`/api/sounds/${query}`)
-				const data: Sound[] | null = await response.json()
-				if (data === null) throw new Error('Failed to fetch sounds.')
-				return data
-			} catch (error) {
-				console.error(error.message)
-				return []
-			}
+		const fetchSounds = async () => {
+			const response = await fetch(`/api/sounds?${query}`)
+			const data = await response.json()
+			setSounds(() => data)
 		}
 
-		getSounds().then((data) => setSounds(() => data))
+		fetchSounds()
 	}, [])
 
-	const playSound = (source_id: string) => {
-		fetch(`api/sounds/uploads/${source_id}`, {
-			headers: { 'Content-Type': 'audio/mpeg' },
-		})
-			.then((res) => res.blob())
-			.then((blob) => {
-				const reader = new FileReader()
-				reader.onloadend = (e) => {
-					new Audio(reader.result?.toString()).play()
-				}
-				reader.readAsDataURL(blob)
-			})
-	}
-
-	function items() {
-		return sounds.map((sound, index) => (
-			<>
-				<p>{sound.name}</p>
-				<p>{sound.author}</p>
-				<button onClick={() => playSound(sound.sourceId)}>🔊</button>
-			</>
-		))
-	}
-
-	return <Catalog items={items()} />
+	return <Catalog sounds={sounds} />
 }
 
-interface Sound {
-	_id: string
-	author: string
-	category: string
-	created: Date
-	description: string
-	fame: number
-	name: string
-	sourceId: string
-	triggers: string[]
+interface SoundsCatalogContainerProps {
+	query?: string
 }
