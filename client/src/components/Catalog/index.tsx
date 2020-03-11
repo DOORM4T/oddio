eee38f029b575b4bca61a9c7c37e1f6213035465
Matt Seto <seanseto@gmail.com>
2020-03-11 00:32:00 -0700

    Empty catalog message

--- client/src/components/Catalog/index.tsx
+++ client/src/components/Catalog/index.tsx
@@ -10,34 +10,41 @@ export default function Catalog({ sounds }: CatalogProps) {
 	const { globalState } = useContext(GlobalContext)
 
 	return (
-		<section className={styles.catalog}>
-			{sounds.length &&
-				sounds.map((sound) => (
-					<div
-						className={styles.item}
-						key={sound._id}
-						data-aos="zoom-in"
-						data-aos-duration={500}
-					>
-						<h2>{sound.name}</h2>
-						<h4>{sound.author}</h4>
-						<p>{sound.description}</p>
-						<p>{sound.category}</p>
-						<p>
-							{sound.triggers.length ? '👄' : ''}
-							{sound.triggers.join(', ')}
-						</p>
-						<p>{new Date(sound.created).toLocaleDateString()}</p>
-						{globalState?.user.username && (
-							<>
-								<AddToSoundboard soundId={sound._id} />
-								<FameButton soundId={sound._id} fame={sound.fame} />
-							</>
-						)}
-						<button onClick={() => playSound(sound.sourceId)}>🔊</button>
-					</div>
-				))}
-		</section>
+		<>
+			{sounds.length > 0 ? (
+				<section className={styles.catalog}>
+					{sounds.map((sound) => (
+						<div
+							className={styles.item}
+							key={sound._id}
+							data-aos="zoom-in"
+							data-aos-duration={500}
+						>
+							<h2>{sound.name}</h2>
+							<h4>{sound.author}</h4>
+							<p>{sound.description}</p>
+							<p>{sound.category}</p>
+							<p>
+								{sound.triggers.length ? '👄' : ''}
+								{sound.triggers.join(', ')}
+							</p>
+							<p>{new Date(sound.created).toLocaleDateString()}</p>
+							{globalState?.user.username && (
+								<>
+									<AddToSoundboard soundId={sound._id} />
+									<FameButton soundId={sound._id} fame={sound.fame} />
+								</>
+							)}
+							<button onClick={() => playSound(sound.sourceId)}>🔊</button>
+						</div>
+					))}
+				</section>
+			) : (
+				<h3 className={styles.empty}>
+					Looks like there's nothing here... yet!
+				</h3>
+			)}
+		</>
 	)
 }
 
