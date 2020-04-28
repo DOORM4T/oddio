bd9a8c3ca1ef926125c3e853e29524e49c278870
Matt Seto <seanseto@gmail.com>
2020-04-27 23:56:48 -0700

    Fix build path for production

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ import express, {
 } from 'express'
 import cookieParser from 'cookie-parser'
 import routes from './routes/index.route'
-import { resolve } from 'path'
+import { join } from 'path'
 // import cors from 'cors'
 // import { PORT } from './index'
 
@@ -17,13 +17,13 @@ app.use(express.json())
 // app.use(cors(/**{ origin: `http://localhost:${PORT}` }**/))
 app.use(cookieParser())
 app.set('view engine', 'ejs')
-app.set('views', resolve(__dirname, '../views'))
+app.set('views', join(__dirname, '../views'))
 app.use('/', routes)
 
 // Serve Client
-app.use(express.static(resolve(process.cwd(), '../build')))
+app.use(express.static(join(__dirname, '../build')))
 app.use('/*', (req: Request, res: Response, next: NextFunction) => {
-	res.sendFile(resolve(process.cwd(), '../build/index.html'))
+	res.sendFile(join(__dirname, 'index.html'))
 	// res.status(404).send('Page not found')
 })
 
