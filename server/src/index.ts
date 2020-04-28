015ca28110366116f783f993f07e07968972af1f
Matt Seto <seanseto@gmail.com>
2020-04-27 23:09:26 -0700

    fix environment variables for dev/prod

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
-require('dotenv').config()
 import app from './app'
 import injectMongoIntoModels from './db'
+
+if (process.env.NODE_ENV === 'development') require('dotenv').config()
+if (!process.env.MONGO_URI) {
+	console.error('Missing environment variables.')
+	process.exit(1)
+}
 
 export const PORT = process.env.PORT || 3001
 const mongoURI = process.env.MONGO_URI || ''
