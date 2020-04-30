aaa06189fc459e0c9aa69c8400087a3da7b753b6
Matt Seto <seanseto@gmail.com>
2020-04-29 23:13:09 -0700

    Fix build path for Heroku deployment.

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,7 @@ import express, {
 } from 'express'
 import cookieParser from 'cookie-parser'
 import routes from './routes/index.route'
-import { join } from 'path'
+import { resolve } from 'path'
 // import cors from 'cors'
 // import { PORT } from './index'
 
@@ -16,14 +16,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // app.use(cors(/**{ origin: `http://localhost:${PORT}` }**/))
 app.use(cookieParser())
-app.set('view engine', 'ejs')
-app.set('views', join(__dirname, '../views'))
+// app.set('view engine', 'ejs')
+// app.set('views', resolve(__dirname, '../views'))
 app.use('/', routes)
 
 // Serve Client
-app.use(express.static(join(__dirname, '../build')))
+app.use(express.static(resolve(__dirname, '../build')))
 app.use('/*', (req: Request, res: Response, next: NextFunction) => {
-	res.sendFile('index.html')
+	res.sendFile(resolve(__dirname, '../build/index.html'))
 	// res.status(404).send('Page not found')
 })
 
