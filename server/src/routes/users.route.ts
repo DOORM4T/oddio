23e6ab896ea7dced6a0feb1dd8a9152c5883311a
Matt Seto <seanseto@gmail.com>
2020-10-30 23:23:49 -0700

    user authentication for looking up users

--- server/src/routes/users.route.ts
+++ server/src/routes/users.route.ts
@@ -3,8 +3,8 @@ import UsersController from '../controllers/UsersController'
 import validate from './middleware/validationMiddleware'
 const router = Router()
 
-router.get('/', UsersController.getUsers)
-router.get('/:username', UsersController.getUserByUsername)
+// router.get('/', UsersController.getUsers)
+router.get('/:username', validate, UsersController.getUserByUsername)
 
 router.post(
 	'/:username/soundboards/create',
